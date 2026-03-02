730b1ca91685374d95f323e1489ea2538e09ee83
Anthony Mesa <admesa@protonmail.com>
2026-03-02 08:30:23 -0500

    flake.nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+
+      projectDir = "$HOME/projects/muser";
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -20,6 +22,14 @@
         ];
 
         shellHook = ''
+          function build() {
+            currDir="$(pwd)";
+            cd "${projectDir}/build"; 
+            cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../;
+            make .;
+            cd $currDir;
+          }
+
           PS1="(dev)$PS1"
         '';
       };
